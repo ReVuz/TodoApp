7107c2df0733f63339559e3aac2097c592973b9c
ReVuz <revathymr916@gmail.com>
2024-03-08 23:54:15 +0530

    basic todo app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
   return (
     <div className="App">
       <div className="mainHeading">
         <h1>TODO APP</h1>
       </div>
-      <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Type Anything</p>
-          </div>
-          <div className="right">
-          </div>
-        </div>
+      <div className='input'>
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type='text' placeholder='Type Anything'></input>
+        <button onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} >Finish</button>
       </div>
-    </div>
+      <div className="todos">
+        {toDos.map((obj) => {
+          return (< div className="todo">
+            <div className="left">
+              <input onChange={(e) => {
+                console.log(e.target.value)
+                console.log(obj)
+                setToDos(toDos.filter(obj2 => {
+                  if (obj2.id === obj.id) {
+                    obj2.status = e.target.checked
+                  }
+                  return obj2
+                }))
+              }} value={obj.status} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+            </div>
+          </div>
+          )
+        })}
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return (<h1>{obj.text}</h1>)
+          }
+          return null
+        })}
+      </div>
+    </div >
   );
 }
 
