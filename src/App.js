45b2769197546fb99a5bfe31c2d25e474df69853
ReVuz <revathymr916@gmail.com>
2024-03-08 23:24:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mainHeading">
+        <h1>TODO APP</h1>
+      </div>
+      <div className="todos">
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>Type Anything</p>
+          </div>
+          <div className="right">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
