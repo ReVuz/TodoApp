1ffd28da63509ae5a6574e17ace4090f4a27c429
ReVuz <revathymr916@gmail.com>
2024-03-25 01:01:06 +0530

    user updation succesfull

--- src/Edit.jsx
+++ src/Edit.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-// import fetchData from './fetchData';
+import { supabase } from './lib/helper/supabaseClient';
+import fetchData from './fetchData';
 
 function Edit({ setToDos, toDos, todoToEdit }) {
     const [toDo, setToDo] = useState('')
@@ -13,7 +14,7 @@ function Edit({ setToDos, toDos, todoToEdit }) {
         }
     }, [todoToEdit]);
 
-    const handleAdd = () => {
+    const handleEdit = () => {
         if (toDo.trim() === '') {
             return;
         }
@@ -22,19 +23,37 @@ function Edit({ setToDos, toDos, todoToEdit }) {
             // Update existing task
             setToDos(toDos.map((obj) => (obj.id === todoToEdit.id ? { ...obj, text: toDo, isEditing: false, id: Date.now() } : obj)));
             setToDo('');
+            editTodo(todoToEdit)
+            fetchData(setToDos);
         } else {
             // Add new task
             setToDos([...toDos, { id: Date.now(), text: toDo, status: false, isEditing: false }]);
             setToDo('');
         }
+
     };
+    async function editTodo(todoToEdit) {
+        try {
+            const { error } = await supabase
+                .from('Todo_list')
+                .update({ Todo: toDo, created_at: new Date().toISOString() })
+                .eq('id', todoToEdit.id)
+                .select();
+
+            if (error) {
+                console.error('Error updating todo:', error);
+            }
+        } catch (error) {
+            console.error('Error updating todo:', error);
+        }
+    }
 
     return (
         <div className='flex justify-center p-5'>
             <form className="w-full max-w-sm" onSubmit={(event) => event.preventDefault()}>
                 <div className="flex items-center border-b border-teal-500 py-2">
                     <input className="text-lg font-medium appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" value={toDo} onChange={(e) => setToDo(e.target.value)} placeholder='Update Task'></input>
-                    <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button" onClick={handleAdd}>
+                    <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button" onClick={handleEdit}>
                         Done
                     </button>
                 </div>
